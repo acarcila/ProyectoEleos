9cfbb608e2731927eb3cc1b0652e8da0ffb5f73a
Nicksiuxs <nicolas.londono27@gmail.com>
2020-03-21 21:43:36 -0500

    Se cró la escena de pruebas y se realizó la fisica de salto de personaje actualizando el prefab

---
+++ ProyectoEleos/Assets/Characters/Teseo/GroundedController.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GroundedController : MonoBehaviour
+{
+    [SerializeField] private int groundMask;
+    [SerializeField] private PlayerController playerController;
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.layer == groundMask) 
+        {
+            playerController.setIsGrounded(true);
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.gameObject.layer == groundMask)
+        {
+            playerController.setIsGrounded(false);
+        }
+    }
+
+}
