8373e9547d598082eb164a0bdf21af1f467f825b
nicksiuxs <nicksiuxs@gmail.com>
2020-03-26 12:37:45 -0500

    Se crearon los metodos para aumentar y disminuir dañor, vida y energia del personaje

--- ProyectoEleos/Assets/Characters/Teseo/Scripts/StatsController.cs
+++ ProyectoEleos/Assets/Characters/Teseo/Scripts/StatsController.cs
@@ -8,6 +8,31 @@ public class StatsController : MonoBehaviour
     private int maxHealth;
     private int energy;
     private int maxEnergy;
+    private int damage;
+    private float tiempo;
+
+    private void Start()
+    {
+        health = 50;
+        maxHealth = 100;
+        energy = 50;
+        maxEnergy = 100;
+        damage = 50;
+    }
+
+    // probar los métodos, solo cambiar el nombre del metodo, cada 5 seg se llama el metodo
+    private void Update()
+    {
+
+        tiempo += Time.deltaTime;
+
+        if (tiempo >= 4f)
+        {
+            decreaseDamage(5);
+            Debug.Log(damage);
+            tiempo = 0;
+        }
+    }
 
     public void setHealth(int health)
     {
@@ -49,4 +74,82 @@ public class StatsController : MonoBehaviour
         return maxEnergy;
     }
 
+    public void setDamage(int damage)
+    {
+        this.damage = damage;
+    }
+
+    public int getDamage()
+    {
+        return damage;
+    }
+
+    public void increaseHealth(int valueIncrease)
+    {
+        if (health >= 0 && health <= maxHealth)
+        {
+            health += valueIncrease;
+        }
+
+        if (health >= maxHealth)
+        {
+            health = maxHealth;
+        }
+    }
+
+    public void decreaseHealth(int valueDecrease)
+    {
+        if (health > 0)
+        {
+            health -= valueDecrease;
+        }
+
+        if (health <= 0)
+        {
+            die();
+        }
+    }
+
+    public void increaseEnergy(int valueIncrease)
+    {
+        if (energy >= 0 && energy <= maxEnergy)
+        {
+            energy += valueIncrease;
+        }
+
+        if (energy > maxEnergy)
+        {
+            energy = maxEnergy;
+        }
+    }
+
+    public void decreaseEnergy(int valueDecrease)
+    {
+        if (energy > 0)
+        {
+            energy -= valueDecrease;
+        }
+        if (energy <= 0)
+        {
+            Debug.Log("sin energía");
+        }
+    }
+
+    public void increaseDamage(int valueIncrease)
+    {
+        damage += valueIncrease;
+    }
+
+    public void decreaseDamage(int valueDecrease)
+    {
+        if (damage > 0)
+        {
+            damage -= valueDecrease;
+        }
+    }
+
+    public void die()
+    {
+        Debug.Log("Mori");
+    }
 }
