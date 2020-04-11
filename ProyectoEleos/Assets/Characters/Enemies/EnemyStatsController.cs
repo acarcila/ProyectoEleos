59b1707d179ef109f535fd215713c9c8296f4dbc
nicksiuxs <nicksiuxs@gmail.com>
2020-04-10 19:02:05 -0500

    Se creó el script de los stats de los enemigos

---
+++ ProyectoEleos/Assets/Characters/Enemies/EnemyStatsController.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyStatsController : MonoBehaviour
+{
+    [SerializeField] private int health;
+    [SerializeField] private int damage;
+
+    public void setHealth(int health)
+    {
+        this.health = health;
+    }
+
+    public int getHealth()
+    {
+        return health;
+    }
+
+    public void setDamage(int damage)
+    {
+        this.damage = damage;
+    }
+
+    public int getDamage()
+    {
+        return damage;
+    }
+}
