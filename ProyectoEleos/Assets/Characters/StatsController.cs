bc9f8f061502fc65e5df0e942e2816eb9d0c4743
acarcila <acarcila98@gmail.com>
2020-04-13 21:57:34 -0500

    Se volvió el StatsController en el controlador general de estadisticas para los personajes

---
+++ ProyectoEleos/Assets/Characters/StatsController.cs
@@ -0,0 +1,143 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StatsController : MonoBehaviour
+{
+    [SerializeField] private int health;
+    [SerializeField] private int maxHealth;
+    [SerializeField] private int energy;
+    [SerializeField] private int maxEnergy;
+    [SerializeField] private int damage;
+    private float tiempo; // borrar después
+
+    // probar los métodos, solo cambiar el nombre del metodo, cada 5 seg se llama el metodo
+    private void Update()
+    {
+    }
+
+    public void setHealth(int health)
+    {
+        this.health = health;
+    }
+
+    public int getHealth()
+    {
+        return health;
+    }
+
+    public void setMaxHealth(int maxHealth)
+    {
+        this.maxHealth = maxHealth;
+    }
+
+    public int getMaxHealth()
+    {
+        return maxHealth;
+    }
+
+    public void setEnergy(int energy)
+    {
+        this.energy = energy;
+    }
+
+    public int getEnergy()
+    {
+        return energy;
+    }
+
+    public void setMaxEnergy(int maxEnergy)
+    {
+        this.maxEnergy = maxEnergy;
+    }
+
+    public int getMaxEnergy()
+    {
+        return maxEnergy;
+    }
+
+    public void setDamage(int damage)
+    {
+        this.damage = damage;
+    }
+
+    public int getDamage()
+    {
+        return damage;
+    }
+
+    public void increaseHealth(int increaseValue)
+    {
+        if (health >= 0 && health <= maxHealth)
+        {
+            health += increaseValue;
+        }
+
+        if (health >= maxHealth)
+        {
+            health = maxHealth;
+        }
+    }
+
+    public void decreaseHealth(int decreaseValue)
+    {
+        if (health > 0)
+        {
+            health -= decreaseValue;
+        }
+
+        if (health <= 0)
+        {
+            health = 0;
+            die();
+        }
+    }
+
+    public void increaseEnergy(int increaseValue)
+    {
+        if (energy >= 0 && energy <= maxEnergy)
+        {
+            energy += increaseValue;
+        }
+
+        if (energy > maxEnergy)
+        {
+            energy = maxEnergy;
+        }
+    }
+
+    public void decreaseEnergy(int decreaseValue)
+    {
+        if (energy > 0)
+        {
+            energy -= decreaseValue;
+        }
+        if (energy <= 0)
+        {
+            energy = 0;
+            Debug.Log("sin energía");
+        }
+    }
+
+    public void increaseDamage(int increaseValue)
+    {
+        damage += increaseValue;
+    }
+
+    public void decreaseDamage(int valueDecrease)
+    {
+        if (damage > 0)
+        {
+            damage -= valueDecrease;
+        }
+        if (damage <= 0)
+        {
+            damage = 0;
+        }
+    }
+
+    public void die()
+    {
+        Debug.Log("Mori");
+    }
+}
