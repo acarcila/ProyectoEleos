e7cbeaea7a1047725c82e0049531dd7b3402e38f
acarcila <acarcila98@gmail.com>
2020-04-13 21:56:46 -0500

    Se programó el hitbox behaviour

---
+++ ProyectoEleos/Assets/Characters/HitboxBehaviour.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HitboxBehaviour : MonoBehaviour
+{
+    [SerializeField] private string otherTag;
+    [SerializeField] private StatsController statsController;
+
+    void OnTriggerStay2D(Collider2D other)
+    {
+        if (other.gameObject.tag == otherTag)
+        {
+            Debug.Log(other.name + " stay");
+            //hacer daño
+            other.gameObject.GetComponent<CharacterController>().takeDamage(statsController.getDamage());
+        }
+    }
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject.tag == otherTag)
+        {
+            Debug.Log(other.name + " enter");
+            //hacer daño
+            other.gameObject.GetComponent<CharacterController>().takeDamage(statsController.getDamage());
+        }
+    }
+}
