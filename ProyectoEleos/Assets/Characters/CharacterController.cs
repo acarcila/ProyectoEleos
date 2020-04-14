9debff4efe7e175340f7fdd0722b1821fcbad306
acarcila <acarcila98@gmail.com>
2020-04-13 21:40:01 -0500

    Se unió el EnemyStatsController en el StatsController

---
+++ ProyectoEleos/Assets/Characters/CharacterController.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class CharacterController : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        toStart();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        toUpdate();
+    }
+
+    void FixedUpdate()
+    {
+        toFixedUpdate();
+    }
+
+    public abstract void takeDamage(int damage);
+    public abstract void toStart();
+    public abstract void toUpdate();
+    public abstract void toFixedUpdate();
+}
