ea8c133e931a7f357978dd5363cf8d84453f75b2
acarcila <acarcila98@gmail.com>
2020-04-16 00:17:14 -0500

    Se creó la carpeta de scripts general y se creó el efecto parallax para los objetos del background

---
+++ ProyectoEleos/Assets/Scripts/ParallaxBehaviour.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ParallaxBehaviour : MonoBehaviour
+{
+    [SerializeField] private float parallaxSpeedX;
+    [SerializeField] private float parallaxSpeedY;
+    [SerializeField] private Transform mainCamera;
+
+    private Vector2 cameraStartPosition;
+    private Vector2 startPosition;
+
+    void Start()
+    {
+        cameraStartPosition = mainCamera.transform.position;
+        startPosition = transform.position;
+    }
+
+    void Update()
+    {
+        // transform.localPosition = new Vector2(startPosition.x - (mainCamera.transform.position.x * parallaxSpeedX), startPosition.y - (mainCamera.transform.position.y * parallaxSpeedY));
+        Vector2 cameraCurrentPosition = mainCamera.transform.position;
+        Vector2 delta = cameraStartPosition - cameraCurrentPosition;
+
+        transform.localPosition = new Vector2(startPosition.x - (delta.x * parallaxSpeedX), startPosition.y - (delta.y * parallaxSpeedY));
+    }
+}
